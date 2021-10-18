bde6830650d23f0766c3070bc760084648234364
Jake Lee <jake.lee@fdmgroup.com>
2021-10-18 14:35:24 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
+import csv
+
 def main():
     #write your code below this line
+    file_name = input("Name of the file:")
+    try:
+        f = open(file_name, "r")
+        reader = csv.reader(f, delimiter = ",")
+        for row in reader:
+            if int(row[1]) > 1:
+                print(row[0] + ", age: " + row[1] + " years")
+            else:
+                print(row[0] + ", age: " + row[1] + " year")
+    except:
+        print("Error")
 
 if __name__ == '__main__':
     main()
